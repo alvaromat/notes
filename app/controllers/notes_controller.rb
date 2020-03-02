2219fee72c8ef790267e1a0826e9c7ad05f751d5
Álvaro Mateos <alvaro_mat@yahoo.es>
2020-03-02 21:40:43 +0100

    Create notes
    Error handling. Page refreshed on create.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,22 @@
 class NotesController < ApplicationController
   def index
-
+    @note = Note.new
   end
+
+  def create
+    @note = Note.create(note_params)
+    @note.user=current_user
+    if @note.save
+      redirect_to index
+    else
+      render partial: 'shared/errors', locals: {errors: @note.errors}, status: :unprocessable_entity
+    end
+  end
+
   before_action :authenticate_user!
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :body, :color)
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,8 @@
 class Note < ApplicationRecord
+  COLORS = [%w(Yellow #FFF176), %w(Green #AED581), %w(Pink #F06292)]
+
+  validates :title, presence: true, length: {minimum: 3}
+  validates :body, presence: true
+  validates :color, presence: true
   belongs_to :user
 end
