bfe3e75e91421da23f1707bbcce14e8d5522a8fc
Álvaro Mateos <alvaro_mat@yahoo.es>
2020-03-02 21:39:38 +0100

    Add jquery and bulma for handling ajax calls and give styles.

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem "bulma-rails", "~> 0.8.0"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+const $ = require("jquery")
+require("bulma")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +16,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', () => {
+    $(document).on("ajax:error", ({detail: [data, status, xhr]}) => {
+        $('#errors').html(xhr.response)
+    })
+});
